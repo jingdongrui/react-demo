d92a583a00a20e7b7c1fecc0bf2ff0c53896f4cd
jingdongrui <jingdr18@qq.com>
2023-05-10 17:59:31 +0800

    chore:update

--- src/components/Todos/index.tsx
+++ src/components/Todos/index.tsx
@@ -4,7 +4,9 @@ import FormControl from 'react-bootstrap/FormControl';
 import InputGroup from 'react-bootstrap/InputGroup';
 import Navbar from 'react-bootstrap/Navbar';
 import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 import React, { useState } from "react";
+import type { FormEvent } from "react";
 
 interface TodosProps {
   id: number,
@@ -13,14 +15,28 @@ interface TodosProps {
 }
 
 const Todos: React.FC<{ data: TodosProps[] }> = (props) => {
-  console.log(1);
+  // console.log("Todos渲染了！", new Date());
   const { data } = props
-  let temp = ""
-  // let addValue: string = ""
-  const [addValue, setAddValue] = useState("")
+  const [todoList, setTodoList] = useState(data)
 
-  const add = () => {
-    console.log(temp);
+  const addTask = (userInput: string) => {
+    setTodoList([...todoList, {
+      id: todoList.length + 1,
+      title: userInput,
+      completed: false,
+    }])
+  }
+  const removeTask = (taskid: number) => {
+    const index = todoList.findIndex(item => item.id === taskid)
+    if (index === -1) return false
+    todoList.splice(index, 1)
+    setTodoList([...todoList])
+  }
+  const handleCheck = (taskid: number, isCompleted: boolean) => {
+    console.log(taskid);
+    const index = todoList.findIndex((item) => item.id === taskid)
+    todoList[index].completed = !todoList[index].completed
+    setTodoList([...todoList])
   }
 
   return (
@@ -31,22 +47,40 @@ const Todos: React.FC<{ data: TodosProps[] }> = (props) => {
         </Container>
       </Navbar>
       <Container>
+        <TodosForm addTask={addTask} />
+        {todoList.map((item) => (
+          <TodosItem data={item} key={item.id} onDelete={removeTask} handleCheck={handleCheck} />
+        ))}
+      </Container >
+    </>
+  )
+}
+interface TodosFormProps {
+  addTask: (userInput: string) => void
+}
+const TodosForm: React.FC<TodosFormProps> = (props) => {
+  const { addTask } = props
+  const [userInput, setUserInput] = useState("")
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault()
+    addTask(userInput)
+    setUserInput("")
+  }
+
+  return (
+    <>
+      <Form onSubmit={handleSubmit} >
         <InputGroup className="mb-2 mt-2">
           <FormControl
             placeholder="do something"
-            defaultValue={addValue}
-            onChange={(e) => { temp = e.target.value }}
+            value={userInput}
+            onChange={(e) => setUserInput(e.target.value)}
           />
-          <Button variant="outline-secondary" onClick={() => add()}>
+          <Button type="submit" variant="outline-secondary">
             ADD
           </Button>
         </InputGroup>
-        {data.map((item) => (
-          <TodosItem data={item} key={item.id} onDelete={() => {
-            console.log(222);
-          }} />
-        ))}
-      </Container >
+      </Form>
     </>
   )
 }
@@ -57,18 +91,20 @@ interface TodosItemProps {
     title: string,
     completed: boolean,
   }
-  onDelete: () => void
+  onDelete: (taskid: number) => void
+  handleCheck: (taskid: number, isCompleted: boolean) => void
 }
 
 const TodosItem: React.FC<TodosItemProps> = (props) => {
-  const { data } = props
+  // console.log("TodosItem渲染了！", new Date());
+  const { data, onDelete, handleCheck } = props
 
   return (
     <>
       <InputGroup className="item" key={data.id}>
-        <InputGroup.Checkbox defaultChecked={data.completed} />
-        <FormControl defaultValue={data.title} style={{ textDecoration: data.completed ? "line-through 2px" : "none" }} />
-        <Button variant="danger">del</Button>
+        <InputGroup.Checkbox defaultChecked={data.completed} onChange={() => handleCheck(data.id, data.completed)} />
+        <FormControl disabled defaultValue={data.title} style={{ textDecoration: data.completed ? "line-through 2px" : "none" }} />
+        <Button variant="danger" onClick={() => onDelete(data.id)}>del</Button>
       </InputGroup>
     </>
   )
